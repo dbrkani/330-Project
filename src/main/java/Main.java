70e7909a4b746e1a1d7dd5f410973a7443584088
Arek <arekcala@gmail.com>
2024-05-03 09:18:18 -0400

    Created base for GUI Class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Casino casino = new Casino();
-        casino.chooseGame();
+        //Casino casino = new Casino();
+        //casino.chooseGame();
+
+        Gui defaultWindow = new Gui();
+
+        defaultWindow.createInitialWindow();
 
     }
 

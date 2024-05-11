4442d242a92f4b5e168ea2d451aeaf620f406c30
Dashi <dbrkani@gmail.com>
2024-05-11 18:31:21 -0400

    Added documentation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,7 @@
+/**
+ * Starts and executes the game logic. Implementations of this method should define the specific actions and rules that constitute the game.
+ */
 public interface Game {
-    /**
-     * Starts and executes the game logic. Implementations of this method should define the specific actions and rules that constitute the game.
-     */
+
     void play();
 }
